e3ca4f0f42ddb4dcab6f4ac086401b5624ec3d5a
hbycgithub2 <1311768852@qq.com>
2025-03-11 14:43:28 +0800

    Feign_1#Feign的使用

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -12,15 +13,19 @@ public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+   // @Autowired
+   // private RestTemplate restTemplate;
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // TODO 2.查询用户
+       /* // TODO 2.查询用户
         String url = "http://userservice/user/" +  order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        User user = restTemplate.getForObject(url, User.class);*/
+        //2.用Feign远程调用
+        User user = userClient.findById(order.getUserId());
         // 3.封装user信息
         order.setUser(user);
         // 4.返回
